8fdab4c91806c0e9fb270acd39ec36b4c5e5802d
Ahmed Maher <ahmedelseaidy22@gmail.com>
2026-09-20 13:11:44 +0300

    Removing enviroment file

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
-const app = require('./app');
+const app = require("./app");
 
-require('dotenv').config();
-const PORT = process.env.PORT;
+const PORT = 4000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
